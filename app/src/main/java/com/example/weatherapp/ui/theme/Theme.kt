0ca1caf0261be8829ccbf7dc11f9675b36efa323
MohamedElquesni <mohamedelquesni@gmail.com>
2026-02-01 00:48:33 +0200

    feat(theme): add light-dark color scheme

--- app/src/main/java/com/example/weatherapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Theme.kt
@@ -12,26 +12,27 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = NightBlack,
+    onBackground = PureWhite,
+
+    surface = NightBlack,
+    onSurface = SkyBlue,
+
+    surfaceVariant = NightBlack,
+    onSurfaceVariant = PureWhite,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    background = SkyBlue,
+    onBackground = NightBlack,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    surface = PureWhite,
+    onSurface = SkyBlue,
+
+    surfaceVariant = PureWhite,
+    onSurfaceVariant = NightBlack,
 )
+
 
 @Composable
 fun WeatherAppTheme(
