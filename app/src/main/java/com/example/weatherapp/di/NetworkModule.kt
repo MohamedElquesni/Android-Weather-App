40c04db8b5db797014a13d041ce2cc49f83b1748
MohamedElquesni <mohamedelquesni@gmail.com>
2026-02-03 12:22:54 +0200

    feat: implement Hilt dependency injection
    - Add Hilt and KSP dependencies to project
    - Create WeatherApplication class with @HiltAndroidApp
    - Create NetworkModule and RepositoryModule for DI
    - Update WeatherViewModel to use @HiltViewModel
    - Update MainActivity with @AndroidEntryPoint
    - Update WeatherScreen to use hiltViewModel()
    - Remove manual ViewModelProvider.Factory

---
+++ app/src/main/java/com/example/weatherapp/di/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.example.weatherapp.di
+
+import com.example.weatherapp.data.api.WeatherApiService
+import com.example.weatherapp.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideWeatherApiService(retrofit: Retrofit): WeatherApiService {
+        return retrofit.create(WeatherApiService::class.java)
+    }
+}
